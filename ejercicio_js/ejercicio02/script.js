477db4ad8ae69588c66c53a4d2ca4fece0382c38
NissaxterDelCFT <111587849+NissaxterDelCFT@users.noreply.github.com>
2022-10-21 11:26:03 -0300

    Add files via upload

---
+++ ejercicio_js/ejercicio02/script.js
@@ -0,0 +1,14 @@
+function datos(){
+    var uni1nota1 = document.getElementsByName("u1r_nota1")[0].value;
+    var uni1nota2 = document.getElementsByName("u1r_nota2")[0].value;
+    var uni1nota3 = document.getElementsByName("u1r_nota3")[0].value;
+
+    var uni2nota1 = document.getElementsByName("u2r_nota1")[0].value;
+    var uni2nota2 = document.getElementsByName("u2r_nota2")[0].value;
+    var uni2nota3 = document.getElementsByName("u2r_nota3")[0].value;
+
+    var uni3nota1 = document.getElementsByName("u3r_nota1")[0].value;
+    var uni3nota2 = document.getElementsByName("u3r_nota2")[0].value;
+    var uni3nota3 = document.getElementsByName("u3r_nota3")[0].value;
+
+}
